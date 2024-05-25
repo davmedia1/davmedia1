efb61e0e2909c375a6119b664146b6b08c2199a3
ANONYMOUS <144067615+davmedia1@users.noreply.github.com>
2024-05-25 11:33:28 +0400

    Update README.m

--- README.m
+++ README.m
@@ -1,10 +1,1 @@
-- 👋 Hi, I’m @davmedia1
-- 👀 I’m interested in learning about the code box of the earth
-- 🌱 I’m currently on the journy 
-- 💞️ I’m looking to collaborate on the best way to figure out what the univers has for the world
-- 📫 How to reach me whatssap+971555568736 call 0565759326
 
-<!---
-davmedia1/davmedia1 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
